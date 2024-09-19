5ea5b52fb82ce4ba144853bcd5727df90f92a71b
Dan <danporeda@yahoo.com>
2024-09-18 23:18:00 -0400

    express.static playing round mit

--- templating_demo/index.js
+++ templating_demo/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const redditData = require('./data.json');
+
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
@@ -16,7 +19,13 @@ app.get('/random', (req, res) => {
 
 app.get('/r/:subreddit', (req, res) => {
   const { subreddit } = req.params;
-  res.render('reddit', { subreddit })
+  const data = redditData[subreddit];
+  if (data) {
+    res.render('reddit', { ...data });
+  } else {
+    res.render('notfound', { subreddit })
+  }
+  
 })
 
 app.get('/cats', (req, res) => {
